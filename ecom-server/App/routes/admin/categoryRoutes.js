b1ef681766fe48eab7206e984a353076c77c0a6d
sultanBytes <sultankhand11@gmail.com>
2024-10-01 14:51:03 +0530

    insert parent category

--- ecom-server/App/routes/admin/categoryRoutes.js
+++ ecom-server/App/routes/admin/categoryRoutes.js
@@ -1,11 +1,12 @@
 let express=require("express");
-const { categoryInsert } = require("../../controller/admin/categoryContoller");
+const { categoryInsert, readParentCategories } = require("../../controller/admin/categoryContoller");
 const { uploads } = require("../../middleware/fileUploadion");
 let categoryRoutes=express.Router();
 
 
 
-categoryRoutes.post('/insert',uploads('uploads/category').single('categoryImage'),categoryInsert)
+categoryRoutes.post('/insert',uploads('uploads/category').single('categoryImage'),categoryInsert);
+categoryRoutes.get('/read-categories', readParentCategories);
 
 
 module.exports={categoryRoutes}
